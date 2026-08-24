9eb54fd4e871cfd7d8f4642a2ee7836a64b7a369
Raphael Rodrigues <Peight-Rapha@users.noreply.github.com>
2026-08-24 09:00:55 -0300

    fix: add landing route

--- brokerages/urls.py
+++ brokerages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import signup
+from .views import landing, signup
 
 
 app_name = 'brokerages'
 
 urlpatterns = [
+    path('', landing, name='landing'),
     path('signup/', signup, name='signup'),
 ]
--- brokerages/views.py
+++ brokerages/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import redirect, render
 from .forms import SignupForm
 
 
+def landing(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard:index')
+    return render(request, 'brokerages/landing.html')
+
+
 def signup(request):
     form = SignupForm(request.POST or None)
     if request.method == 'POST' and form.is_valid():
         user = form.save()
         login(request, user)
-        return redirect('health_check')
+        return redirect('dashboard:index')
     return render(request, 'brokerages/signup.html', {'form': form})
